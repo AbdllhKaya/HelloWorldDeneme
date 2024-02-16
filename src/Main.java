53150414a4b43883f8e01e84f8cdf09e116ac0f4
Abdullah KAYA <abdullahky0409@gmail.com>
2024-02-16 21:23:55 +0300

    Main sınıfına işlem eklendi.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+
+        int x=1 ,y=2, t= x+y ;
+        System.out.print(t);
 
 
         }
